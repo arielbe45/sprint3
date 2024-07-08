aac674fd95191074edf0445f4619d50d335f80c9
TLP <uri.carmeli.cohen.45@gmail.com>
2024-07-08 16:29:51 +0300

    WORKING FINAL VERY WORKING CoDE

--- qr_data_to_bytearray.py
+++ qr_data_to_bytearray.py
@@ -1,6 +1,6 @@
 import cv2
 import dir_to_bytes
-
+import base64
 
 global collected_data
 global last_code_index
@@ -11,8 +11,9 @@ last_code_index = -1
 def add_data(qr_data):
     global last_code_index
     global collected_data
-
-    
+    print(qr_data)
+    if len(qr_data) < 2:
+        return False
     code_index = int(qr_data[0])
     code_length = int(qr_data[1])
     data = qr_data[2:code_length+2]
@@ -25,11 +26,12 @@ def add_data(qr_data):
     elif code_index > last_code_index + 1:
         print("Missing QR code(s) in sequence")
     
-    # print(code_index, code_length, data)
 
     # if code_length != 254:
     #     return True
 
 
 def process_data():
-    print(collected_data)
+    decoded_data = base64.b64decode(collected_data)
+    print(decoded_data)
+    dir_to_bytes.bytes_to_folder(decoded_data, 'TestFolder')
