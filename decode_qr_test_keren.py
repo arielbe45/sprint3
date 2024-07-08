b16a080a706697a2c955dafbe95742d6e58dc4b2
Ido Keren <71383536+IdoKeren@users.noreply.github.com>
2024-07-08 15:35:45 +0300

    qr reader

--- decode_qr_test_keren.py
+++ decode_qr_test_keren.py
@@ -1,5 +1,6 @@
 import cv2
 import pyzxing
+import dir_to_bytes
 
 # Initialize the zxing decoder
 reader = pyzxing.BarCodeReader()
@@ -10,8 +11,8 @@ def decode_barcode(image_path):
 
 def main():
     # Open a connection to the webcam (0 is usually the built-in webcam)
-    cap = cv2.VideoCapture(0)
-
+    cap = cv2.VideoCapture(1)
+ 
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         return
@@ -45,27 +46,40 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
+global collected_data
+global last_code_index
 collected_data = bytearray()
-last_code_index = 0
+last_code_index = -1
 
 
 def add_data(qr_data):
+    global last_code_index
+    global collected_data
+
+
     if 'raw' not in qr_data:
-        return
+        return False
     
     data_from_qr = qr_data['raw']
     code_index = int(data_from_qr[0])
     code_length = int(data_from_qr[1])
-    data = data_from_qr[2:code_length]
+    data = data_from_qr[2:code_length+2]
     if code_index == last_code_index:
-        return
+        return False
     elif code_index == last_code_index + 1:
+        print("Got QR number ", code_index)
         last_code_index = code_index
         collected_data.extend(data)
     elif code_index > last_code_index + 1:
         print("Missing QR code(s) in sequence")
+    
+    # print(code_index, code_length, data)
 
-    print(code_index, code_length, data)
+    # if code_length != 254:
+    #     return True
+
+
 
 
 main()
+print(collected_data)
---
+++ qr_data_to_bytearray.py
@@ -0,0 +1,40 @@
+import cv2
+import dir_to_bytes
+
+
+global collected_data
+global last_code_index
+collected_data = bytearray()
+last_code_index = -1
+
+
+def add_data(qr_data):
+    global last_code_index
+    global collected_data
+
+
+    if 'raw' not in qr_data:
+        return False
+    
+    data_from_qr = qr_data['raw']
+    code_index = int(data_from_qr[0])
+    code_length = int(data_from_qr[1])
+    data = data_from_qr[2:code_length+2]
+    if code_index == last_code_index:
+        return False
+    elif code_index == last_code_index + 1:
+        print("Got QR number ", code_index)
+        last_code_index = code_index
+        collected_data.extend(data)
+    elif code_index > last_code_index + 1:
+        print("Missing QR code(s) in sequence")
+    
+    # print(code_index, code_length, data)
+
+    # if code_length != 254:
+    #     return True
+
+
+
+def proccess_data():
+    print(collected_data)
