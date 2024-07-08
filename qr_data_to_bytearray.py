2d09a59a3a699317adfdc7442da1a98877317647
TLP <uri.carmeli.cohen.45@gmail.com>
2024-07-08 17:49:13 +0300

    WORKING FINAL VERY WORKING CoDE

--- qr_data_to_bytearray.py
+++ qr_data_to_bytearray.py
@@ -11,23 +11,27 @@ last_index = -1
 def add_data(qr_data):
     global last_index
     global collected_data
-    print(qr_data)
     if len(qr_data) < 2:
         return False
     code_index = int(qr_data[0])
     code_length = int(qr_data[1])
-    data = qr_data[2:code_length+2]
+    size = 8
+    if code_length!=0:
+        size = code_length
+
+    data = qr_data[2:size+2]
     if (code_index in collected_data):
         return False
     else:
         print("Got QR number ", code_index)
+        print(data)
         collected_data[code_index] = data
     
     if code_length != 0:
         last_index = code_index
         print("Finished pass, missing data: ", get_missing_data())
-        if len(get_missing_data()) == 0:
-            return True
+    if last_index != -1 and len(get_missing_data()) == 0:
+        return True
     
 
 def get_missing_data():
@@ -41,9 +45,10 @@ def process_data():
     global collected_data
     global last_index
     data = bytearray()
-    for i in range(0, last_index):
+    print(collected_data)
+    for i in range(0, last_index+1):
         data.extend(collected_data[i])
-
+    print(data)
     decoded_data = base64.b64decode(data)
     print(decoded_data)
     dir_to_bytes.bytes_to_folder(decoded_data, 'TestFolder')
